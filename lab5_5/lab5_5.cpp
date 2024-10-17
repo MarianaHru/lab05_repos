76c53cddb8cc010345a317febd17ea741be33bfe
Mariana Hrudzinska <grudzinskamarana@gmail.com>
2024-10-17 13:53:39 +0300

    Changed code for lad5

--- lab5_5/lab5_5.cpp
+++ lab5_5/lab5_5.cpp
@@ -1,41 +1,80 @@
+// #include <iostream>
+// using namespace std;
+
+// // // Функція для обчислення біноміального коефіцієнта з рекурсією
+// // int binomialCoefficient(int n, int k, int level, int &maxDepth)
+// // {
+
+// //     if (level > maxDepth)
+// //     {
+// //         maxDepth = level;
+// //     }
+
+// //     cout << "level = " << level << endl;
+// //     if (k == 0 || k == n)
+// //         return 1;
+
+// //     return binomialCoefficient(n - 1, k - 1, level + 1, maxDepth) + binomialCoefficient(n - 1, k, level + 1, maxDepth);
+// // }
+
+// int binomialCoefficient (int k, int n, int &maxDepth, int level)
+// {
+//     if (level > maxDepth)
+//         maxDepth = level;
+//     cout << "level =" << level << endl;
+
+//     if (k == 0 || k == n)
+//         return 1;
+//     else
+//         return binomialCoefficient(n - 1, k - 1, level + 1, maxDepth) + binomialCoefficient(n - 1, k, level + 1, maxDepth);
+// }
+// int main()
+// {
+//     int n, k;
+//     int maxDepth = 0; // Змінна для збереження максимальної глибини рекурсії
+
+//     cout << "Введіть n: ";
+//     cin >> n;
+//     cout << "Введіть k: ";
+//     cin >> k;
+
+//     int result = binomialCoefficient(n, k, 1, maxDepth);
+
+//     cout << "Біноміальний коефіцієнт C(" << n << ", " << k << ") = " << result << endl;
+//     cout << "level: " << maxDepth << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
-// Функція для обчислення біноміального коефіцієнта з рекурсією
-int binomialCoefficient(int n, int k, int level, int &maxDepth)
+int binomialCoefficient(int n, int k, int &maxDepth, int level)
 {
-    // Оновлюємо максимальну глибину, якщо поточний рівень більше
     if (level > maxDepth)
-    {
         maxDepth = level;
-        cout << "level = " << level << endl;
-    }
+    cout << "level = " << level << endl;
 
-    // Базовий випадок: якщо k = 0 або k = n
     if (k == 0 || k == n)
         return 1;
-
-    // Рекурсивний випадок: використовуємо формулу
-    return binomialCoefficient(n - 1, k - 1, level + 1, maxDepth) + binomialCoefficient(n - 1, k, level + 1, maxDepth);
+    else
+        return binomialCoefficient(n - 1, k - 1, maxDepth, level + 1) + binomialCoefficient(n - 1, k, maxDepth, level + 1);
 }
 
 int main()
 {
     int n, k;
-    int maxDepth = 0; // Змінна для збереження максимальної глибини рекурсії
+    int maxDepth = 0;
 
-    // Введення значень n та k
     cout << "Введіть n: ";
     cin >> n;
     cout << "Введіть k: ";
     cin >> k;
 
-    // Виклик функції для обчислення біноміального коефіцієнта
-    int result = binomialCoefficient(n, k, 1, maxDepth); // Початковий рівень = 1
+    int result = binomialCoefficient(n, k, maxDepth, 1);
 
-    // Виведення результату
     cout << "Біноміальний коефіцієнт C(" << n << ", " << k << ") = " << result << endl;
-    cout << "level: " << maxDepth << endl;
+    cout << "Максимальна глибина рекурсії: " << maxDepth << endl;
 
     return 0;
 }
